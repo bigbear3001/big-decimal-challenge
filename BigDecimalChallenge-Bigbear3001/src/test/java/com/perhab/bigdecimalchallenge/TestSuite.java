a463a62324e12b49f6bcbd9f4ea4ecd6c988b9e8
bigbear3001 <bigbear.ap@gmail.com>
2011-10-13 22:06:15 +0200

    added Testsuite for the implementation

---
+++ BigDecimalChallenge-Bigbear3001/src/test/java/com/perhab/bigdecimalchallenge/TestSuite.java
@@ -0,0 +1,20 @@
+package com.perhab.bigdecimalchallenge;
+
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import bigdecimalchallenge.test.InitialisationTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	InitialisationTest.class
+})
+public class TestSuite {
+	
+	@BeforeClass
+	public static void impl() {
+		System.setProperty("bigdecimalchallenge.impl", PerhabBigDecimal.class.getCanonicalName());
+	}
+}
