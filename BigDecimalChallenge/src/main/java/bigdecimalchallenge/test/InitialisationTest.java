cd8351b006166c36fbe3174975bd3adcdbaab28e
Florian Gutmann <f.gutmann@irregular.at>
2011-10-13 23:21:33 +0200

    Added methods for basic arithmetics of unkown Bigdecimals in TestUtils

--- BigDecimalChallenge/src/main/java/bigdecimalchallenge/test/InitialisationTest.java
+++ BigDecimalChallenge/src/main/java/bigdecimalchallenge/test/InitialisationTest.java
@@ -13,30 +13,53 @@ import bigdecimalchallenge.BigDecimal;
 public class InitialisationTest {
 	
 	/**
+	 * Tests integers either positive or negative
+	 * 
+	 * @param positive If the integers should be positive or negative
+	 */
+	public void integer(boolean positive) {
+		// all digits
+		for(String number : Numbers.digits) {
+			assertEquals("Creating number from String " + number + " doesn't equal number.toString()", number, bd.toString());
+		}
+		
+//		// some simple numbers
+//		for (String number : integers) {
+//			if(!positive) {
+//				number = "-" + number;
+//			}
+//			
+//			BigDecimal<?> bd = TestUtils.createInstance(number);
+//			assertEquals("Creating number from String " + number + " doesn't equal number.toString()", number, bd.toString());
+//		}
+//		
+//		// all numbers concatenated together
+//		StringBuilder largeInteger = new StringBuilder();
+//		for(String number : integers) {
+//			largeInteger.append(number);
+//		}
+//		
+//		// double the integer digits every run and test initialisation
+//		for (int i = 0; i < 10; i++) {
+//			BigDecimal<?> bd = TestUtils.createInstance(largeInteger.toString());
+//			assertEquals("Creating number from String " + largeInteger.toString() + " doesn't equal number.toString()", largeInteger.toString(), bd.toString());
+//			largeInteger.append(largeInteger.toString());
+//		}		
+	}
+	
+	/**
 	 * Tests initialization of positive integer values
 	 */
 	@Test
 	public void positiveInteger() {
-		String [] numbers = new String [] {
-			"1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
-			"100", "1000", "1000", "10000", "100000", "100000", "1000000", "1000000",
-		};
-		
-		// some simple numbers
-		for (String number : numbers) {
-			BigDecimal<?> bd = TestUtils.createInstance(number);
-			assertEquals("Creating number from String " + number + " doesn't equal number.toString()", number, bd.toString());
-		}
-		
-		// all numbers concatenated together
-		StringBuilder largeInteger = new StringBuilder();
-		for(String number : numbers) {
-			largeInteger.append(number);
-		}
-		for (int i = 0; i < 10; i++) {
-			BigDecimal<?> bd = TestUtils.createInstance(largeInteger.toString());
-			assertEquals("Creating number from String " + largeInteger.toString() + " doesn't equal number.toString()", largeInteger.toString(), bd.toString());
-			largeInteger.append(largeInteger.toString());
-		}
+		integer(true);
+	}
+	
+	/**
+	 * Tests initialization of negative integers
+	 */
+	@Test
+	public void negativeInteger() {
+		integer(false);
 	}
 }
--- BigDecimalChallenge/src/main/java/bigdecimalchallenge/test/TestUtils.java
+++ BigDecimalChallenge/src/main/java/bigdecimalchallenge/test/TestUtils.java
@@ -32,11 +32,28 @@ public class TestUtils {
 	 * @param number The number for which to create a BigDecimal.
 	 * @return The instance initialized with the number.
 	 */
-	public static BigDecimal<?> createInstance(String number) {
+	@SuppressWarnings("unchecked")
+	public static BigDecimal<? extends BigDecimal<?>> createInstance(String number) {
 		try {
-			return implConstructor.newInstance(number);	
+			return (BigDecimal<? extends BigDecimal<?>>) implConstructor.newInstance(number);	
 		} catch (Exception e) {
 			throw new RuntimeException("Could not instantiate BigDecimal of type " + clazz.getCanonicalName() + " for number " + number, e);
 		}
 	}
+	
+	public static BigDecimal<?> add(BigDecimal one, BigDecimal two) {
+		return (BigDecimal<?>) one.add(two);
+	}
+	
+	public static BigDecimal<?> subtract(BigDecimal one, BigDecimal two) {
+		return (BigDecimal<?>) one.subtract(two);
+	}
+	
+	public static BigDecimal<?> multiply(BigDecimal one, BigDecimal two) {
+		return (BigDecimal<?>) one.multiply(two);
+	}
+		
+	public static BigDecimal<?> divide(BigDecimal one, BigDecimal two) {
+		return (BigDecimal<?>) one.divide(two);
+	}	
 }
