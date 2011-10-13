f99b19faf07fe9cb914b22bd93d21d2af3a73139
Florian Gutmann <f.gutmann@irregular.at>
2011-10-13 21:56:04 +0200

    Implemented tests to read from stystem property

---
+++ BigDecimalChallenge/src/main/java/bigdecimalchallenge/test/TestUtils.java
@@ -0,0 +1,42 @@
+package bigdecimalchallenge.test;
+
+import java.lang.reflect.Constructor;
+
+import bigdecimalchallenge.BigDecimal;
+
+public class TestUtils {
+	
+	static Class<?>clazz;
+	static Constructor<? extends BigDecimal> implConstructor;
+	
+	static {
+		String name = System.getProperty("bigdecimalchallenge.impl");
+		if (name == null) {
+			throw new RuntimeException("You need to define the system property 'bigdecimalchallenge.impl'");
+		}
+		
+		try {
+			clazz = Class.forName(name);
+			if(! BigDecimal.class.isAssignableFrom(clazz)) {
+				throw new Exception("Must implement " + BigDecimal.class.getCanonicalName());
+			}
+			implConstructor = (Constructor<? extends BigDecimal>) clazz.getConstructor(String.class);
+		} catch (Exception e) {
+			throw new RuntimeException("Error initializing BigDecimal implementation " + name, e);
+		}
+	}
+	
+	/**
+	 * Creates a BigDecimal instance for a given number.
+	 * 
+	 * @param number The number for which to create a BigDecimal.
+	 * @return The instance initialized with the number.
+	 */
+	public static BigDecimal createInstance(String number) {
+		try {
+			return implConstructor.newInstance(number);	
+		} catch (Exception e) {
+			throw new RuntimeException("Could not instantiate BigDecimal of type " + clazz.getCanonicalName() + " for number " + number, e);
+		}
+	}
+}
