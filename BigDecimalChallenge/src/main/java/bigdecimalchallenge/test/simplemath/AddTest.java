6f3b5defe7806e044dd0fba38f7bca5a421e5736
bigbear3001 <bigbear.ap@gmail.com>
2011-10-14 19:04:12 +0200

    cleaned up the code

--- BigDecimalChallenge/src/main/java/bigdecimalchallenge/test/simplemath/AddTest.java
+++ BigDecimalChallenge/src/main/java/bigdecimalchallenge/test/simplemath/AddTest.java
@@ -1,18 +1,18 @@
 package bigdecimalchallenge.test.simplemath;
 
 import bigdecimalchallenge.BigDecimal;
+import bigdecimalchallenge.test.BigDecimalTest;
 import bigdecimalchallenge.test.TestUtils;
 
 
-public class AddTest<T extends BigDecimal> {
+public class AddTest extends BigDecimalTest {
 	public void addIntegers() {
 		String[] numbers = new String[]{"1", "2", "3", "9", "99", "100", "999", "3333", "6666", "-9999"};
 		for(int i = 0; i < numbers.length; i++) {
 			for(int j = 0; j < numbers.length; i++) {
-				BigDecimal<T> zero = (BigDecimal<T>) TestUtils.createInstance("0");
-				BigDecimal<T> a = zero.add(numbers[i]);
-				BigDecimal<T> b = TestUtils.createInstance(numbers[i]);
-				BigDecimal<T> result = a.add(b);
+				BigDecimal<Object> a = number(numbers[i]);
+				BigDecimal<Object> b = number(numbers[i]);
+				BigDecimal<Object> result = a.add(b);
 			}
 		}
 	}
