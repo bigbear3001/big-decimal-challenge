af44eec58b74ac7d675e498a7d0b371659c17b33
bigbear3001 <bigbear.ap@gmail.com>
2011-10-14 19:02:48 +0200

    corrected return type definition of the interface

--- BigDecimalChallenge/src/main/java/bigdecimalchallenge/BigDecimal.java
+++ BigDecimalChallenge/src/main/java/bigdecimalchallenge/BigDecimal.java
@@ -19,21 +19,21 @@ public interface BigDecimal<T> {
 	 * @param value - value to add to this big decimal
 	 * @return new big decimal with the added values of this big decimal and the given big decimal
 	 */
-	public T add(T value);
+	public BigDecimal<T> add(T value);
 	
 	/**
 	 * subtract a big decimal from this big decimal
 	 * @param value - value to subtract to this big decimal
 	 * @return new big decimal with the value of this big decimal subtracted by the given big decimal
 	 */
-	public T subtract(T value);
+	public BigDecimal<T> subtract(T value);
 	
 	/**
 	 * divide this big decimal by a big decimal.
 	 * @param value - value to divide this big decimal by
 	 * @return new big decimal with the added values of this big decimal and the given big decimal
 	 */
-	public T divide(T value);
+	public BigDecimal<T> divide(T value);
 	
 	/**
 	 * multiply this big decimal with another big decimal
