6841f939dfe53f551876746a77db37a8a8b9d4f9
bigbear3001 <bigbear.ap@gmail.com>
2011-10-14 19:03:36 +0200

    added simple math tests zu full test suite

--- BigDecimalChallenge/src/main/java/bigdecimalchallenge/test/FullTests.java
+++ BigDecimalChallenge/src/main/java/bigdecimalchallenge/test/FullTests.java
@@ -4,10 +4,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import bigdecimalchallenge.test.simplemath.SimpleMathTests;
+
 import junit.framework.TestSuite;
 @RunWith(Suite.class)
 @SuiteClasses({
-	InitialisationTest.class
+	InitialisationTest.class,
+	SimpleMathTests.class
 })
 
 public class FullTests extends TestSuite {
